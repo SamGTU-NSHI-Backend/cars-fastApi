58c7ffd4bdcdc22d120b7efc5aca4a6c4cd32048
rassafel <rassafel@gmail.com>
2021-10-18 20:20:44 +0400

    Add routing to car controller.

--- app/main.py
+++ app/main.py
@@ -1,38 +1,15 @@
-import logging
-
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import FileResponse
 
-_log_format = f"%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
-
-def get_file_handler():
-    file_handler = logging.FileHandler("logs/x.log")
-    file_handler.setLevel(logging.INFO)
-    file_handler.setFormatter(logging.Formatter(_log_format))
-    return file_handler
-
-def get_stream_handler():
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.INFO)
-    stream_handler.setFormatter(logging.Formatter(_log_format))
-    return stream_handler
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-logger.addHandler(get_file_handler())
-logger.addHandler(get_stream_handler())
+from .routers import car_controller
 
 app = FastAPI()
 
+app.include_router(car_controller.router)
+
 app.mount("/static", StaticFiles(directory="./public/static"), name="static")
 
-@app.get("/")
+@app.get("/", include_in_schema=False)
 def read_index():
-    logger.info("Input path /")
     return FileResponse('./public/index.html')
-
-@app.get("/hello")
-def hello():
-    logger.info("Input path /hello")
-    return {"value": "Hello World"}
